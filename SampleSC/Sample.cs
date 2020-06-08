0cf7dd0270e8550195d7e300c833464a3748edd9
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-08 13:58:49 +0900

    新規登録

---
+++ SampleSC/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+class Sample
+{
+    static Form form;
+    public static void Main()
+    {
+        form = new Form();
+        
+
+        Application.Run(form);
+
+
+    }
+}
