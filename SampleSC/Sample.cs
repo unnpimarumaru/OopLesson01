483404799c734304cb0bab7b2c14b30ce22eb73b
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-08 15:22:26 +0900

    配列*20

--- SampleSC/Sample.cs
+++ SampleSC/Sample.cs
@@ -14,7 +14,22 @@ class Sample:Form
     //デフォルトコンストラクタ
     public Sample()
     {
-       int[] scores = {64, 71, 31, 97, 54};
+       int[]  scores = new int[5];
+
+        //scores[0] = 10;
+        //scores[1] = 20;
+        //scores[2] = 30;
+        //scores[3] = 40;
+        //scores[4] = 50;
+
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i] = (i * 20) + 20;
+
+        }
+
+
 
         for (int i = 0; i < scores.Length; i++)
         {
