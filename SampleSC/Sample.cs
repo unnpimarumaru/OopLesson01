1f3a52e7b9c39d1ff94dc29065fd372a94de1700
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-08 16:01:59 +0900

    値の合計

--- SampleSC/Sample.cs
+++ SampleSC/Sample.cs
@@ -14,32 +14,29 @@ class Sample:Form
     //デフォルトコンストラクタ
     public Sample()
     {
-       int[]  scores = new int[5];
-
+        int[] scores = new int[5];
+        int sum = 0;
         //scores[0] = 10;
         //scores[1] = 20;
         //scores[2] = 30;
         //scores[3] = 40;
         //scores[4] = 50;
 
+        //整数値の取り込み
 
+
+        //値の入力
         for (int i = 0; i < scores.Length; i++)
         {
-            scores[i] = (i * 20) + 20;
+
+            Console.Write("scores[" + i + "] = "); //画面に文字を出力
+
+            scores[i] = int.Parse(Console.ReadLine());//入力値を読み込み
+
+            sum += scores[i];
 
         }
-
-
-
-        for (int i = 0; i < scores.Length; i++)
-        {
-            Console.WriteLine(scores[i]);
-        }
-    
-    
-    
-    
+        Console.WriteLine("入力された値の合計は" + sum + "です");
     }
-
 
 }
