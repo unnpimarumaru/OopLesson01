da55cae4d0afcac88a7d7a380aa78127f2ada4e0
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-09 16:50:15 +0900

    太田

--- SampleSC/Sample.cs
+++ SampleSC/Sample.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Drawing;
 using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 //Formクラスを継承したSampleクラス
-class Sample:Form
+class Sample : Form
 {
-  　
+
     public static void Main()
     {
 
@@ -18,24 +19,26 @@ class Sample:Form
         string moji = "";
 
         //stringの別名（エイリアス）がString
-        Console.Write("文字列：");
-        var str = Console.ReadLine();
-        Console.Write("置換前：");
-        string beformoji = Console.ReadLine();
-        Console.Write("置換後：");
-        string aftermoji = Console.ReadLine();
 
-        var str2 = str.Replace(beformoji, aftermoji);
-        Console.WriteLine(str2);
+        var str = "";
+
+        str = Console.ReadLine();
+        var city = str.Split(',');
+
+        for (int i = 0; i < city.Length; i++)
+        {
+            Console.Write("群馬県");
+            Console.WriteLine(city[i] + "市");
 
 
-
+        }
     }
+}
 
     
 
 
-}
+
 
 
 
