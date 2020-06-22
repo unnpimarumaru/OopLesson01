895e55e79d4cf520945bd435e943755ce593dedd
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-22 13:37:39 +0900

    0.348を定数に変更

---
+++ DistanceConverter/FeetConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter
+{
+    //フィートとメートルの単位変換クラス
+    public static class FeetConverter
+    {
+        //private const double raito = 0.3048;
+
+        public static readonly  double ratio = 0.3048;
+
+        //メートルからフィートを求める（静的メソッドとして定義）
+        public static double FromMeter(double meter)
+        {
+            return meter / 0.3048;
+        }
+
+        //フィートからメートルを求める（静的メソッドとして定義）
+        public static double ToMeter(double feet)
+        {
+            return feet * 0.3048;
+        }
+    }
+}
---
+++ DistanceConverter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter
+{
+    class Program
+    {
+        //-tom フィートからメートルへの対応表
+        //-tof メートルからフィートへの対応表
+        static void Main(string[] args)
+        {
+            if(args.Length >= 1 && args[0] == "-tom" )
+            {
+                //フィートからメートルへの対応表を出力
+                PrintFeetToMeterList(5, 50);
+            }
+            else  
+            {
+                //メートルからフィートへの対応表
+                PrintMeterToFeetList(10, 100);
+            }            
+        }
+
+        //フィートからメートルへの対応表を出力
+        static void PrintFeetToMeterList(int start, int stop)
+        {
+            for (int feet = start; feet <= stop; feet++)
+            {                
+                double meter = FeetConverter.ToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+
+        //メートルからフィートへの対応表を出力
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = FeetConverter.ToMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
+        }
+    }
+}
