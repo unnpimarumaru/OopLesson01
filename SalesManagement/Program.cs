e458f4888961236ef096efa34735ca08fd137807
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-23 17:51:26 +0900

    メソッドを移動

--- SalesManagement/Program.cs
+++ SalesManagement/Program.cs
@@ -15,53 +15,18 @@ namespace SalesManagement
 
         static void Main(string[] args)
         {
-            Dictionary<string, int> stores = new Dictionary<string, int>();
+            SalesCounter sales = new SalesCounter(("Sales.csv"));
 
-            SalesCounter sales = new SalesCounter(ReadSales("Sales.csv"));
+            Dictionary<string, int>  amountPerStore = sales.GetPerStoreSales();
 
-            foreach (var item in sales._sales)
-            {
-            
-                if (stores.ContainsKey(item.ShopName))
-                {
-                    stores[item.ShopName] += item.Amount;
-                }
-                else
-                {
-                    stores[item.ShopName] = item.Amount;
-                }
-
-            }
-            foreach (var item in stores)
+            foreach (var item in amountPerStore)
             {
                 Console.WriteLine($"{item.Key}は{item.Value}です。");
             }
-                
-            
+
+
         }
 
-        //売上データを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath)
-        {
-            //売上データを入れるオブジェクトを生成
-            var sales = new List<Sale>();
-            string[] lines = File.ReadAllLines(filePath);
-            //読み込んだ数の行だけ繰り返す
-            foreach (string line in lines)
-            {
-                string[] items = line.Split(',');
-
-                //オブジェクト初期化子
-                Sale sale = new Sale
-                {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale); //saleオブジェクトをリストに追加
-            }
-            return sales;
-        }
 
     }
 }
--- SalesManagement/SalesCounter.cs
+++ SalesManagement/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,63 @@ namespace SalesManagement
     //売上集計クラス
     class SalesCounter
     {
-        public List<Sale> _sales;
+        private List<Sale> _sales;
         //コンストラクタ
-        public SalesCounter(List<Sale> sales)
+        public SalesCounter(string path)
         {
-            _sales = sales;
+            _sales = ReadSales(path);
+        }
+
+        //店舗別売り上げを求める
+        public Dictionary<string,int> GetPerStoreSales()
+        {
+            Dictionary<string, int> stores = new Dictionary<string, int>();
+
+
+
+            foreach (var item in _sales)
+            {
+
+                if (stores.ContainsKey(item.ShopName))
+                {
+                    stores[item.ShopName] += item.Amount;
+                }
+                else
+                {
+                    stores[item.ShopName] = item.Amount;
+                }
+
+            }
+            
+            return stores;
+
+
+
         }
 
 
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        private static List<Sale> ReadSales(string filePath)
+        {
+            //売上データを入れるオブジェクトを生成
+            var sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            //読み込んだ数の行だけ繰り返す
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+
+                //オブジェクト初期化子
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale); //saleオブジェクトをリストに追加
+            }
+            return sales;
+        }
 
 
     }
