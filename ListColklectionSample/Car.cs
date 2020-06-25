eface9bedff877aec40eab2f23ee05eea5846290
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-25 15:24:38 +0900

    Carクラス作成

---
+++ ListColklectionSample/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListColklectionSample
+{
+    class Car
+    {
+        //社名
+        public string Name { get; set; }
+        //メーカー
+        public string Maker { get; set; }
+        //カテゴリー
+        public string Category { get; set; }
+    }
+}
