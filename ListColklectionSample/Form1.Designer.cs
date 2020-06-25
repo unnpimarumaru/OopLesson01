b5edef3623a43bc6e7836cc65f9df1338392b0c5
KouheiTakahashi <31174@ojs.ac.jp>
2020-06-25 15:36:54 +0900

    フォーム画面作成

--- ListColklectionSample/Form1.Designer.cs
+++ ListColklectionSample/Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbMaker = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbCategory = new System.Windows.Forms.TextBox();
+            this.dvgCarData = new System.Windows.Forms.DataGridView();
+            this.btAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCarData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(27, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "車名";
+            // 
+            // tbName
+            // 
+            this.tbName.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbName.Location = new System.Drawing.Point(116, 39);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(293, 31);
+            this.tbName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(26, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "メーカー";
+            // 
+            // tbMaker
+            // 
+            this.tbMaker.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbMaker.Location = new System.Drawing.Point(116, 95);
+            this.tbMaker.Name = "tbMaker";
+            this.tbMaker.Size = new System.Drawing.Size(293, 31);
+            this.tbMaker.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(13, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 24);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "カテゴリ";
+            // 
+            // tbCategory
+            // 
+            this.tbCategory.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbCategory.Location = new System.Drawing.Point(116, 148);
+            this.tbCategory.Name = "tbCategory";
+            this.tbCategory.Size = new System.Drawing.Size(293, 31);
+            this.tbCategory.TabIndex = 2;
+            // 
+            // dvgCarData
+            // 
+            this.dvgCarData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgCarData.Location = new System.Drawing.Point(12, 208);
+            this.dvgCarData.Name = "dvgCarData";
+            this.dvgCarData.RowTemplate.Height = 21;
+            this.dvgCarData.Size = new System.Drawing.Size(514, 230);
+            this.dvgCarData.TabIndex = 3;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btAdd.Location = new System.Drawing.Point(427, 122);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(99, 57);
+            this.btAdd.TabIndex = 5;
+            this.btAdd.Text = "登録";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(543, 450);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.dvgCarData);
+            this.Controls.Add(this.tbCategory);
+            this.Controls.Add(this.tbMaker);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "車登録アプリ";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCarData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbMaker;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbCategory;
+        private System.Windows.Forms.DataGridView dvgCarData;
+        private System.Windows.Forms.Button btAdd;
     }
 }
 
